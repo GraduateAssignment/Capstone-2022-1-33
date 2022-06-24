99f07c0e0932b5912778ff6830dbe48aa909c5fb
jyoung <juyong4190@daum.net>
2022-06-24 17:45:10 +0900

    프로젝트 초기 설정: navigation / toolbar

---
+++ SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.pnu.smartwalkingstickapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.pnu.smartwalkingstickapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+
+
+}
