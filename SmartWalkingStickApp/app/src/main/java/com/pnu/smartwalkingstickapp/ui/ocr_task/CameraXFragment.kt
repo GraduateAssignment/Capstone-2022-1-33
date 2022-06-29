fe7a7bfaf1189ef4b0a7c4afea91e94c3cd1e13c
jyoung <juyong4190@daum.net>
2022-06-29 16:14:55 +0900

    카메라: 미리보기 / 촬영 구현 완료

---
+++ SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/ui/ocr_task/CameraXFragment.kt
@@ -0,0 +1,131 @@
+package com.pnu.smartwalkingstickapp.ui.ocr_task
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.pnu.smartwalkingstickapp.MainActivity
+import com.pnu.smartwalkingstickapp.R
+import com.pnu.smartwalkingstickapp.databinding.FragmentCameraXBinding
+import com.pnu.smartwalkingstickapp.databinding.FragmentOcrBinding
+import java.io.File
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraXFragment : Fragment() {
+    private lateinit var binding: FragmentCameraXBinding
+
+    private lateinit var _viewFinder: PreviewView
+    private lateinit var preview: Preview
+    private lateinit var imageCapture: ImageCapture
+
+    private lateinit var safeContext: Context
+
+    private lateinit var outputDirectory: File
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        safeContext = context
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCameraXBinding.inflate(inflater, container, false)
+        _viewFinder = binding.viewFinder
+        binding.cameraButton.setOnClickListener { takePhoto() }
+        outputDirectory = getOutputDirectory()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        when(val cameraPermission = ContextCompat.checkSelfPermission(safeContext, Manifest.permission.CAMERA)){
+            PackageManager.PERMISSION_GRANTED ->  startCamera()
+            else -> requestPermission()
+        }
+    }
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.CAMERA), 99)
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(safeContext)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            imageCapture = ImageCapture.Builder().build()
+
+            preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(_viewFinder.surfaceProvider)
+            }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch(exception: Exception) {
+                Log.e(TAG, "Use case binding failed", exception)
+            }
+        }, ContextCompat.getMainExecutor(safeContext))
+    }
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+        // Create timestamped output file to hold the image
+        val photoFile = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(safeContext), object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+            }
+
+            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo capture succeeded: $savedUri"
+                Toast.makeText(safeContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+        })
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else activity?.filesDir!!
+    }
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+}
