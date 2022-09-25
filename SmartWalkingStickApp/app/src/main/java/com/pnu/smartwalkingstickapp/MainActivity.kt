144976ee267150fe0bdd7fcb7a6fb913ed0767c7
jyoung <juyong4190@daum.net>
2022-09-25 16:36:11 +0900

    Make emergency call and utils

--- SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/MainActivity.kt
+++ SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.pnu.smartwalkingstickapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -18,6 +25,7 @@ import com.pnu.smartwalkingstickapp.ui.map_task.MapViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navHostFragment: NavHostFragment
     val mapViewModel : MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
@@ -36,5 +44,24 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{ controller, destination, arguments ->
+            when (destination.id) {
+                R.id.nav_camera_x_fragment -> binding.bottomNav.visibility = View.GONE
+                else -> binding.bottomNav.visibility = View.VISIBLE
+            }
+        }
+        when(val phonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)){
+            PackageManager.PERMISSION_GRANTED ->  null
+            else -> requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 99)
+    }
+
+    fun getForegroundFragment(): Fragment? {
+        return navHostFragment.childFragmentManager.fragments[0]
     }
 }
