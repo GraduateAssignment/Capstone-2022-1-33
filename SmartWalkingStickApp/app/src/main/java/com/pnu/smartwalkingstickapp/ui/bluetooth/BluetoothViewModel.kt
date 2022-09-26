afadaa3f660b6777fd770298cffd7d3a838f8a02
jyoung <juyong4190@daum.net>
2022-09-26 19:44:47 +0900

    Update Design and Code

--- SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/ui/bluetooth/BluetoothViewModel.kt
+++ SmartWalkingStickApp/app/src/main/java/com/pnu/smartwalkingstickapp/ui/bluetooth/BluetoothViewModel.kt
@@ -1,45 +1,24 @@
 package com.pnu.smartwalkingstickapp.ui.bluetooth
 
-import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.NavHostFragment
-import com.pnu.smartwalkingstickapp.ui.map_task.MapFragment
-import com.pnu.smartwalkingstickapp.ui.map_task.ShowDirectionFragment
-import com.pnu.smartwalkingstickapp.ui.ocr_task.OcrFragment
 
-class BluetoothViewModel(): ViewModel() {
-    private var navHostFragment: NavHostFragment? = null
+class BluetoothViewModel() : ViewModel() {
 
     private val _onReceiveRunEmergencyCall = MutableLiveData<Boolean>(false)
     val onReceiveRunEmergencyCall: LiveData<Boolean> = _onReceiveRunEmergencyCall
 
-    private val _onReceiveRunCamera = MutableLiveData<Boolean>(false)
-    val onReceiveRunCamera: LiveData<Boolean> = _onReceiveRunCamera
+    private val _onReceiveRunCamera = MutableLiveData<String>("")
+    val onReceiveRunCamera: LiveData<String> = _onReceiveRunCamera
 
-    fun runTask(tag: String) {
-        val topFragment: Fragment? = navHostFragment?.childFragmentManager?.fragments?.get(0)
-        val bundle = bundleOf("feature" to tag)
-        when (topFragment) {
-            is MapFragment -> (topFragment as MapFragment).actionToCameraXFragment(bundle)
-            is OcrFragment -> (topFragment as OcrFragment).actionToCameraXFragment(bundle)
-            is BluetoothFragment -> (topFragment as BluetoothFragment).actionToCameraXFragment(bundle)
-            is ShowDirectionFragment ->(topFragment as ShowDirectionFragment).actionToCameraXFragment(bundle)
-            else -> null
-        }
-    }
 
     fun runEmergencyCall() {
         _onReceiveRunEmergencyCall.postValue(true)
     }
 
-    fun runCamera() {
-        _onReceiveRunCamera.postValue(true)
+    fun runCamera(feature: String) {
+        _onReceiveRunCamera.postValue(feature)
     }
 
-    fun navHostFragment(_navHostFragment: NavHostFragment){
-        navHostFragment = _navHostFragment
-    }
 }
